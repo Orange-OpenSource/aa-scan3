1ee52d3379f87f3f93b282c3e1a052d255454e58
Yann E. MORIN <yann.morin@orange.com>
2020-07-17 12:31:13 +0200

    aa-scan3: categorise plugins
    Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

--- aa_scan3/plugins/bar.py
+++ aa_scan3/plugins/bar.py
@@ -7,3 +7,17 @@ class Scanner:
     def __init__(self, parser):
         parser.add_argument('--hello', metavar='WHO', default='world',
                             help='Say hello to WHO.')
+
+    def once(self, path):
+        self.logger('bar called once for {}'.format(path))
+        self.profile.add_path(path, 'r')
+        return []
+
+    def scan(self, path):
+        return []
+
+    def mangle(self, path):
+        return path
+
+    def emit(self, path):
+        return path
--- aa_scan3/plugins/foo.py
+++ aa_scan3/plugins/foo.py
@@ -6,3 +6,6 @@ Hello there, my foo...
 class Scanner:
     def __init__(self, parser):
         pass
+
+    def once(self, path):
+        return []
