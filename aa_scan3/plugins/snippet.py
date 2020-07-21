b80addf991da2076331cbe6e6b8209a8cfa71c4b
Yann E. MORIN <yann.morin@orange.com>
2020-07-21 13:16:56 +0200

    aa-scan3/plugins: add snippet plugin
    Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

---
+++ aa_scan3/plugins/snippet.py
@@ -0,0 +1,63 @@
+"""
+Test if the file has associated AppArmor snippets in .aa files.
+
+A snippet file contains one or more pattern-rules, one per line.
+No sanity-check is done on the file, and each line must end with
+a new-line '\\n' character (even the last one).
+
+The snippet plugin also has (very crude) support for child
+profiles. Child profiles are detected as a line starting with
+the keyword 'profile' followed by a path and ending with an
+opening curly brace, and extend until the first closing curly
+brace. Everything in-between is copied as-is without any mangling
+at all (except leading spaces are added or removed to maintain
+a consistent indentation in the generated file).
+"""
+
+
+import glob
+import os
+import re
+
+
+class Scanner:
+    def __init__(self, parser):
+        parser.add_argument('--enable', '--disable', default='--enable',
+                            action=parser.ToggleAction(['--enable']),
+                            help='Enable or disable snippets.')
+        parser.add_argument('--file', metavar='FILE',
+                            action='append', default=[],
+                            help='Read rules and capabilities from the specified file.'
+                            + ' This option can be specified more than once, in which'
+                            + ' case all files will be used. This option is not'
+                            + ' impacted by the --disable option.')
+
+    def once(self, path):
+        for snippet in self.file:
+            yield from self.read_one_snippet(snippet)
+
+    def scan(self, path):
+        if not self.enable:
+            return
+        for snippet in glob.glob(os.path.join(self.staging_dir, path[1:] + '.aa*')):
+            yield from self.read_one_snippet(snippet)
+
+    def read_one_snippet(self, snippet):
+        self.logger('>>{}<<'.format(snippet))
+        with open(snippet, 'r') as f:
+            for l in (l.strip().rstrip(',') for l in f):
+                if l.startswith('/'):
+                    path, mode = re.split(' +', l)
+                    self.profile.add_path(path, mode)
+                    if 'm' in mode:
+                        yield path
+                elif l.startswith('capability '):
+                    _, cap = re.split(' +', l)
+                    self.profile.add_capability(cap)
+                elif l.startswith('network '):
+                    _, domain, proto = re.split(' +', l)
+                    self.profile.add_network(domain, proto)
+                elif l.startswith('profile '):
+                    self.profile.start_child_profile(re.split(' +', l)[-2])
+                elif l.startswith('}'):
+                    self.profile.end_child_profile()
